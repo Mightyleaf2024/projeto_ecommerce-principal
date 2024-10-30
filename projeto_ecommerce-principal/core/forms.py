48538520620af285b6637523825f35ceecf84d21
Mightyleaf2024 <mightyleaf24@gmail.com>
2024-10-30 09:31:30 -0300

    Delete projeto_ecommerce-principal directory

--- projeto_ecommerce-principal/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Produtos
-
-class ProdutosModelForm (forms.ModelForm):
-    class Meta:
-        model = Produtos
-        fields = ['codigo', 'nome', 'descricao', 'quantidade', 'valor', 'imagem']
-
